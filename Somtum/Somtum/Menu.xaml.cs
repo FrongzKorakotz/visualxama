aa988cffd49574a4014461aaa250dcde92b0836b
FrongzKorakotz <korakot0082@gmail.com>
2020-03-28 12:41:32 +0700

    xamasomtum

---
+++ Somtum/Somtum/Menu.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Somtum
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : ContentPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+        private void buybutton_clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Success());
+        }
+    }
+}
